54c4f99b89d8df00624601b7fce5c28eef3bf901
Labib <labibfaisal450@gmail.com>
2026-08-15 12:37:03 +0600

    tests: added clone tests

--- internal/infra/github/github_repo_test.go
+++ internal/infra/github/github_repo_test.go
@@ -3,9 +3,11 @@ package github
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/labib0x9/sockforces/config"
 )
 
@@ -16,6 +18,10 @@ func TestGithubRepo(t *testing.T) {
 	repo := NewGithubRepo(client, cnf.Github)
 	username := "labib0x9"
 	repoName := fmt.Sprintf("tcp-echo-server-%s-intrigation-test", username)
+	cloneRepo := "ZERO9xz/Submission-labib0x9-tcp-echo-server-template"
+	id := uuid.New().String()
+	var path string
+	var err error
 
 	t.Run("Create Repository", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -35,12 +41,29 @@ func TestGithubRepo(t *testing.T) {
 		}
 	})
 
+	t.Run("Clone Repository", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		path, err = repo.CloneRepository(ctx, cloneRepo, id)
+		if err != nil {
+			t.Fatalf("Clone Repository Failed: %v\n", err)
+		}
+	})
+
 	t.Cleanup(func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 		_, err := client.Repositories.Delete(ctx, cnf.Github.Org, repoName)
 		if err != nil {
 			t.Fatalf("Cleanup Failed: %v\n", err)
+		}
+	})
+
+	t.Cleanup(func() {
+		err := os.RemoveAll(path)
+		if err != nil {
+			t.Fatalf("Cloned Repo (tmpDir) Cleanup Failed: %v\n", err)
 		}
 	})
 }
